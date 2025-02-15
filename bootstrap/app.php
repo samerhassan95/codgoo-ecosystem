4b7ca1c6e7c56094554ac5fa6693ed57736bb730
fatma mohamed <fatmamohamed2101@gmail.com>
2025-02-15 13:00:08 +0200

    admin auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'client' => \App\Http\Middleware\Client::class,
-            '\admin' => \App\Http\Middleware\Admin::class,
+            'admin' => \App\Http\Middleware\Admin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
