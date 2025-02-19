ce679e8d4b6eb1177d8d0a7f63895624713d4f14
fatma mohamed <fatmamohamed2101@gmail.com>
2025-02-19 19:21:05 +0200

    amount

--- app/Models/Milestone.php
+++ app/Models/Milestone.php
@@ -46,6 +46,7 @@ class Milestone extends Model
                     'project_id' => $milestone->project_id,
                     'status' => 'unpaid',
                     'due_date' => now()->addDays(30),
+                    'amount' => $milestone->amount,
                 ]);
             }
         });
