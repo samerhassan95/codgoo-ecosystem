66df398b92004d39b50c8fe5508624ab8f835440
fatma mohamed <fatmamohamed2101@gmail.com>
2025-02-20 16:01:05 +0200

    fillable

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -9,7 +9,7 @@ class Notification extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'message', 'token', 'is_read'];
+    protected $fillable = ['title', 'message', 'token', 'is_read','notifiable_id','notifiable_type'];
 
     public function notifiable()
     {
