bf6d68b2cf240ab55be326f099bacfa0f6b7e062
fatma mohamed <fatmamohamed2101@gmail.com>
2025-03-09 14:16:10 +0200

    'Token is Invalid'

--- app/Http/Middleware/Client.php
+++ app/Http/Middleware/Client.php
@@ -53,7 +53,7 @@ class Client
             if ($e instanceof TokenInvalidException ) {
                 return response()->json([
                     'status'=>false,
-                    'message'=>$e->getMessage(),
+                    'message'=>'Token is Invalid',
                 ],400);
             } else if ($e instanceof TokenExpiredException) {
                 return response()->json([
