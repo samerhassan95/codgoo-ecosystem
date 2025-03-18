7579c7d2cfc5ab80d76ae9b1ea54a2a2942a6c10
fatma mohamed <fatmamohamed2101@gmail.com>
2025-03-18 02:13:12 +0200

    employee app migration

---
+++ database/migrations/2025_03_16_133900_create_achievements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained('tasks')->onDelete('cascade');
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->foreignId('attendance_id')->constrained('attendances')->onDelete('cascade');
+            $table->text('achievement_description');
+            $table->timestamp('submitted_at')->useCurrent();
+            $table->foreignId('achievement_type')->constrained('achievement_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achievements');
+    }
+};
