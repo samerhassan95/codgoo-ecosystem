73f0c4b8f7fd79d1e235e90ce5742d5bff30cc36
fatma mohamed <fatmamohamed2101@gmail.com>
2025-07-29 13:07:55 +0300

    ..

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,69 +1,5 @@
 <?php
 
-// namespace App\Http\Controllers;
-
-// use Illuminate\Http\Request;
-// use App\Repositories\NotificationRepository;
-// use App\Services\FirebaseService;
-// use App\Models\Admin;
-// use App\Models\Client;
-
-// class NotificationController extends Controller
-// {
-//     protected $notificationRepository;
-//     protected $firebaseService;
-
-//     public function __construct(NotificationRepository $notificationRepository, FirebaseService $firebaseService)
-//     {
-//         $this->notificationRepository = $notificationRepository;
-//         $this->firebaseService = $firebaseService;
-//     }
-
-//     public function sendNotification(Request $request)
-//     {
-//         $request->validate([
-//             'title' => 'required|string',
-//             'message' => 'required|string',
-//             'notifiable_id' => 'nullable|integer',
-//             'notifiable_type' => 'nullable|in:admin,client',
-//         ]);
-
-//         if ($request->has('notifiable_id') && $request->has('notifiable_type')) {
-//             $model = $request->notifiable_type === 'admin' ? Admin::class : Client::class;
-//             $notifiable = $model::find($request->notifiable_id);
-
-//             if (!$notifiable || !$notifiable->fcm_token) {
-//                 return response()->json(['message' => 'User not found or missing FCM token'], 400);
-//             }
-
-//             // Send Notification via Firebase
-//             $this->firebaseService->sendNotification($notifiable->fcm_token, $request->title, $request->message);
-
-//             // Store notification in the database
-//             $this->notificationRepository->createNotification($notifiable, $request->title, $request->message, $notifiable->fcm_token);
-
-//             return response()->json(['message' => 'Notification sent to user and stored successfully!']);
-//         }
-
-//         $clients = Client::whereNotNull('fcm_token')->get();
-
-//         if ($clients->isEmpty()) {
-//             return response()->json(['message' => 'No clients with valid FCM tokens.'], 400);
-//         }
-
-//         foreach ($clients as $client) {
-//             $this->firebaseService->sendNotification($client->fcm_token, $request->title, $request->message);
-
-//             // Store notification in the database for each client
-//             $this->notificationRepository->createNotification($client, $request->title, $request->message, $client->fcm_token);
-//         }
-
-//         return response()->json(['message' => 'Notifications sent to all clients and stored successfully!']);
-//     }
-
-// }
-
-
 namespace App\Http\Controllers;
 
 use App\Http\Resources\NotificationResource;
@@ -75,8 +11,9 @@ use App\Services\FirebaseService;
 use App\Models\Admin;
 use App\Models\Client;
 use App\Models\NotificationTemplate;
-// use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
+
 class NotificationController extends Controller
 {
     protected $notificationRepository;
